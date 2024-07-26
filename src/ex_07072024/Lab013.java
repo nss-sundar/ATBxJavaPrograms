b54c2485e987bad832a11642acbbb8352e605688
nss-sundar <nsssnn.qa@gmail.com>
2024-07-26 20:42:36 +0530

    Lab excercise partd

---
+++ src/ex_07072024/Lab013.java
@@ -0,0 +1,11 @@
+package ex_07072024;
+
+public class Lab013 {
+    //can i run the class without main method: No,
+    // because you can't see RUN option
+
+    public static void main(String[] args) {
+
+    }
+
+    }
---
+++ src/ex_07072024/Lab014.java
@@ -0,0 +1,10 @@
+package ex_07072024;
+
+public class Lab014 {
+// check is it correct .. note square braces
+    public static void main(String args[]) {
+
+        System.out.println("same");
+
+    }
+}
